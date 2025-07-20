14683be715f83ca5c5fbbd38f65cfbad44519015
Julie-Charles16 <juliecharles2716@gmail.com>
2025-07-20 22:26:40 +0200

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <>
+      <div className="fixed w-full h-20 flex justify-between items-center px-4 bg-slate-900 text-gray-300">
+        <button>Home</button>
+        <button>About</button>
+        <button>Projects</button>
+        <button>Contacts</button>
+      </div>
+    </>
+  )
+}
